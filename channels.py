7f027f3832bed849bb26fdddad6d98b0427ea726
Tommy Fisher <tommybfisher@gmail.com>
2018-01-16 14:43:30 +0200

    Added separate Python scripts

---
+++ channels.py
@@ -0,0 +1,68 @@
+# encoding: utf-8
+
+import sys
+from workflow import Workflow, web
+import urllib
+
+api_url = 'https://api.elisaviihde.fi/etvrecorder/ajaxprograminfo.sl'
+
+def main(wf):
+
+    channels = [
+        'Yle TV1',
+        'Yle TV2',
+        'MTV3',
+        'Nelonen',
+        'Sub',
+        'Kutonen',
+        'FOX',
+        'AVA',
+        'Yle Teema & Fem',
+        'Liv',
+        'Jim',
+    ]
+
+    for channel in channels:
+        now_and_next_progs = wf.cached_data(
+            'now_and_next_' + channel,
+            lambda: get_now_and_next_program(channel),
+            max_age=60
+        )
+        
+        wf.add_item(
+            title=channel,
+            subtitle=urllib.unquote(
+                now_and_next_progs
+            ).decode('utf8'),
+            arg=channel,
+            valid=True,
+            icon='icons/{}.png'.format(channel)
+        )
+
+    # Send the results to Alfred as XML
+    wf.send_feedback()
+
+
+def get_now_and_next_program(channel):
+    params = {'24h': channel}
+    r = web.get(api_url, params)
+
+    r.raise_for_status()
+
+    result = r.json()
+
+    programs = result['programs']
+
+    now_and_next_progs = '{} - {} - {}'.format(
+        programs[0]['name'].encode('utf-8'),
+        programs[0]['simple_end_time'],
+        programs[1]['name'].encode('utf-8'),
+    )
+
+    return now_and_next_progs
+
+
+if __name__ == u"__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
+
---
+++ programs.py
@@ -0,0 +1,48 @@
+# encoding: utf-8
+
+import sys
+from workflow import Workflow, web
+import urllib
+
+api_url = 'https://api.elisaviihde.fi/etvrecorder/ajaxprograminfo.sl'
+
+def main(wf):
+    channel = sys.argv[1]
+
+    programs = wf.cached_data(
+        channel + ' programs',
+        lambda: get_programs(channel),
+        max_age=60
+    )
+
+    for program in programs:
+        wf.add_item(
+            title=urllib.unquote(
+                program['name'].encode('utf-8')
+            ).decode('utf8'),
+            subtitle='{} - {}'.format(
+                program['simple_start_time'], program['simple_end_time']
+            ),
+            valid=True,
+            icon='icons/{}.png'.format(channel)
+        )
+
+    # Send the results to Alfred as XML
+    wf.send_feedback()
+
+
+def get_programs(channel):
+    params = {'24h': channel}
+    r = web.get(api_url, params)
+
+    r.raise_for_status()
+
+    result = r.json()
+
+    return result['programs']
+
+
+if __name__ == u"__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
+
